b19000076eae68083c0e07a081a6217ac21825ca
Jahongir <jahongirq702@gmail.com>
2024-05-29 00:23:07 +0500

    123

--- signs/urls.py
+++ signs/urls.py
@@ -2,6 +2,26 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    # path('category/', ListCategoryAPIView.as_view(), name='category'),
+    # path('road-signs/<int:pk>/', ListRoadSignsAPIView.as_view(), name='road-signs'),
+    # path('create/', CreateListSignsView.as_view(), name='create-list-signs'),
+    # path('update/<int:pk>/', RetrieveUpdateDestroyView.as_view(), name='update-list-signs'),
+    # path('delete/<int:pk>/', RetrieveUpdateDestroyView.as_view(), name='delete-list-signs'),
+    # path('get/<int:pk>/', RetrieveUpdateDestroyView.as_view(), name='get-list-signs'),
+    # path('patch/<int:pk>/', RetrieveUpdateDestroyView.as_view(), name='patch-list-signs'),
+   
+    # path('delete/<int:pk>/', delete, name='delete'),
+    # path('get/<int:pk>/', get, name='get'),
+    # path('patch/<int:pk>/', patch, name='patch'),
+    # path('create/', create, name='create'),
+    # path('put/', put, name='put'),
+
     path('category/', ListCategoryAPIView.as_view(), name='category'),
     path('road-signs/<int:pk>/', ListRoadSignsAPIView.as_view(), name='road-signs'),
+    path('create/', CreateAPIView.as_view(), name='create'),
+    path('update/<int:pk>/', UpdateAPIView.as_view(), name='update'),
+    path('delete/<int:pk>/', DeleteAPIView.as_view(), name='delete'),
+    path('get/<int:pk>/', GetAPIView.as_view(), name='get'),
+    path('patch/<int:pk>/', PatchUpdateAPIView.as_view(), name='patch'),
+
 ]
--- signs/views.py
+++ signs/views.py
@@ -1,8 +1,10 @@
 from django.contrib.messages import success
 from django.shortcuts import render
 from rest_framework import status
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
 
 from .serializers import *
 from.models import *
@@ -32,5 +34,105 @@ class ListRoadSignsAPIView(APIView):
                 'status_code': status.HTTP_200_OK
 
             })
+
+
+class CreateAPIView(APIView):
+    def post(self, request):
+        serializer = RoadSignsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
+
+class UpdateAPIView(APIView):
+    def put(self, request, pk):
+        road_signs = RoadSigns.objects.get(pk=pk)
+        serializer = RoadSignsSerializer(road_signs, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DeleteAPIView(APIView):
+    def delete(self, pk):
+        road_signs = RoadSigns.objects.get(pk=pk)
+        road_signs.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+class GetAPIView(APIView):
+    def get(self, request, pk):
+        road_signs = RoadSigns.objects.get(pk=pk)
+        serializer = RoadSignsSerializer(road_signs)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class PatchUpdateAPIView(APIView):
+    def patch(self, request, pk):
+        road_signs = RoadSigns.objects.get(pk=pk)
+        serializer = RoadSignsSerializer(road_signs, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        
+# class CreateListSignsView(ListCreateAPIView):
+#     queryset = RoadSigns.objects.all()
+#     serializer_class = RoadSignsSerializer
+#
+#
+# class RetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+#     queryset = RoadSigns.objects.all()
+#     serializer_class = RoadSignsSerializer
+
+# @api_view(['POST'])
+# def create(request):
+#     serializer = RoadSignsSerializer(data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+# @api_view(['PUT'])
+# def put(request):
+#     road_signs = RoadSigns.objects.all()
+#     serializer = RoadSignsSerializer(road_signs, data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+# @api_view(['DELETE'])
+# def delete(pk):
+#     road_signs = RoadSigns.objects.get(pk=pk)
+#     road_signs.delete()
+#     return Response(status=status.HTTP_204_NO_CONTENT)
+#
+# @api_view(['GET'])
+# def get(request, pk):
+#     road_signs = RoadSigns.objects.get(pk=pk)
+#     serializer = RoadSignsSerializer(road_signs, many=False)
+#     return Response(serializer.data, status=status.HTTP_200_OK)
+#
+#
+# @api_view(['PATCH'])
+# def patch(request, pk):
+#     road_signs = RoadSigns.objects.get(pk=pk)
+#     serializer = RoadSignsSerializer(road_signs, data=request.data, partial=True)
+#     if serializer.is_valid():
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
 
 
